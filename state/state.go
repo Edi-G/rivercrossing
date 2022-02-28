ce237112e3dcbd35084bcc0d0281a538fb0ab5e0
Edi-G <edi2001@live.no>
2022-02-28 22:57:42 +0100

    first commit

---
+++ state/state.go
@@ -0,0 +1,11 @@
+package state
+
+func ViewState() string {
+    // Sjekke data som er lagret ("kylling til venstre", "rev til venstre")
+    return "[kylling rev korn hs ---\\ \\__/ _________________/---]"
+}
+
+func PutKyInBoat() string {
+    // Sjekke data som er lagret ("kylling til venstre", "rev til venstre")
+    return "[rev korn hs ---\\ \\kylling/ _________________/---]"
+}
---
+++ state/state_test.go
@@ -0,0 +1,21 @@
+// Package for testing the represention of the state of the riverworld
+package state
+
+import "testing"
+
+// Test function implemented in line with the Golang's testing tool
+func TestViewState(t *testing.T) {
+    wanted := "[kylling rev korn hs ---\\ \\__/ _________________/---]"
+    state := ViewState();
+    if state != wanted {
+         t.Errorf("Feil, fikk %q, ønsket %q.", state, wanted)
+    }
+}
+
+func TestPutKyInBoat(t *testing.T) {
+    wanted := "[rev korn hs ---\\ \\kylling/ _________________/---]"
+    state := PutKyInBoat();
+    if state != wanted {
+         t.Errorf("Feil, fikk %q, ønsket %q.", state, wanted)
+    }
+}
